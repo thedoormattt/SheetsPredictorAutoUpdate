f21e1e0ccdf2134f303e41dec7995ba1d6c3c68e
Matthew Woodburn <mwoodburn95@gmail.com>
2022-10-01 14:38:04 +0100

    Initial commit

---
+++ updateResults.py
@@ -0,0 +1,123 @@
+import requests
+import json
+from googleapiclient import discovery
+from google.oauth2 import service_account
+import os
+import logging
+
+logging.basicConfig(filename="autoUpdate.log", format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
+
+
+def get_results():
+    logging.info("Getting latest results from football-data.org")
+
+    uri = "https://api.football-data.org/v4/competitions/PL/matches?season=2022&status=FINISHED"
+    api_key = generate_football_data_credentials()
+    headers = {"X-Auth-Token": api_key}
+
+    res_raw = requests.get(uri, headers=headers)
+    res_extract = res_raw.json()["matches"]
+
+    return res_extract
+
+
+def generate_football_data_credentials():
+    file = open("football-data-credentials.json")
+    data = json.load(file)["api_key"]
+    file.close()
+
+    return data
+
+
+def generate_google_credentials():
+    scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file",
+              "https://www.googleapis.com/auth/spreadsheets"]
+    secret_file = os.path.join(os.getcwd(), 'google-credentials.json')
+    credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
+
+    return credentials
+
+
+def get_sheets_results(sheet_range: str):
+    logging.info("Getting latest scores from Google Sheets")
+
+    spreadsheet_id = "11EmqTM3AMsaeZ3h9tYVb0MNfUCVPNe-y9SlRolCHp2E"
+    credentials = generate_google_credentials()
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+
+    auth_res_raw = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=sheet_range).execute()
+    auth_res_filtered = auth_res_raw["values"]
+
+    return auth_res_filtered
+
+
+def post_updated_results(sheet_range: str, updated_results: list):
+    logging.info("Posting updated scores to Google Sheets")
+
+    spreadsheet_id = "11EmqTM3AMsaeZ3h9tYVb0MNfUCVPNe-y9SlRolCHp2E"
+    data = {"values": updated_results}
+    credentials = generate_google_credentials()
+
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+
+    res = service.spreadsheets().values()\
+        .update(spreadsheetId=spreadsheet_id, body=data, range=sheet_range, valueInputOption="USER_ENTERED").execute()
+
+    print(res)
+
+
+# For testing against a json file
+def get_results_file(filename: str):
+    file = open(filename)
+    data = json.load(file)
+    file.close()
+
+    return data
+
+
+# To reconcile team name difference between the two data sources, allowing them to be searched against each other
+def format_team_names(orig_name: str):
+    if orig_name == "Brighton Hove":
+        return "Brighton"
+    elif orig_name == "Leeds United":
+        return "Leeds"
+    elif orig_name == "Leicester City":
+        return "Leicester"
+    elif orig_name == "Man United":
+        return "Man Utd"
+    elif orig_name == "Nottingham":
+        return "Nottingham Forest"
+    elif orig_name == "Tottenham":
+        return "Spurs"
+    elif orig_name == "Wolverhampton":
+        return "Wolves"
+    else:
+        return orig_name
+
+
+def update_scores(results, matches):
+    updated_matches = matches
+    for match in updated_matches:
+        if len(match) == 2:
+            for result in results:
+                if match[0] == format_team_names(result["homeTeam"]["shortName"]) \
+                        and match[1] == format_team_names(result["awayTeam"]["shortName"]):
+                    res = str(result["score"]["fullTime"]["home"]) + "-" + str(result["score"]["fullTime"]["away"])
+                    match.append(res)
+    return updated_matches
+
+
+range_name_test: str = "AutoUpdateTest!D3:F382"
+range_name: str = "EngineRoom!F4:H383"
+
+# Get results and matches to be updated
+fd_res_live_test = get_results()
+gs_res_live_test = get_sheets_results(range_name_test)
+
+# Update Google Sheets object with the latest scores
+updated = update_scores(fd_res_live_test, gs_res_live_test)
+
+# Post updated object to Google Sheets.
+#   This will appear to update the entire 380 game list, but if there are no scores available it will only 'update'
+#   the team names
+post_updated_results(range_name_test, updated)
